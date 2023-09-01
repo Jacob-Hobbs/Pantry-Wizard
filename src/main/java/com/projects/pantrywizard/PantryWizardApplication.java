e95034404b7bf16aa5dbbe83c3c5dd96b7ce8206
Jacob Hobbs <JacobRayHobbs@gmail.com>
2023-08-31 19:34:49 -0500

    Fourth commit 08/31
    Sample data for ingredients now loading into database. May need to consider getting rid of auto-incrementing ingredientId as primary key. After sample data is loaded, auto-increment picks up from 101 and then continues every 50 integers after. Just something to consider.

--- src/main/java/com/projects/pantrywizard/PantryWizardApplication.java
+++ src/main/java/com/projects/pantrywizard/PantryWizardApplication.java
@@ -3,11 +3,14 @@ package com.projects.pantrywizard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PantryWizardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PantryWizardApplication.class, args);
+
+
 	}
 
 }
--- src/main/java/com/projects/pantrywizard/controller/IngredientController.java
+++ src/main/java/com/projects/pantrywizard/controller/IngredientController.java
@@ -1,39 +1,64 @@
 package com.projects.pantrywizard.controller;
 
+import com.projects.pantrywizard.dao.IngredientRepository;
 import com.projects.pantrywizard.entity.Ingredient;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 @Controller
 @RequestMapping("/")
 public class IngredientController {
 
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
     private List<Ingredient> localIngredientList;
 
-    // define @PostConstruct to load the student data at runtime
+    // define @PostConstruct to load the sample ingredient data at runtime
     @PostConstruct
-    public void loadSampleData() {
+    public void loadSampleData() throws IOException {
+
+        //ingredientRepository.deleteAll();
+
 
         // Initialize list as empty
         localIngredientList = new ArrayList<>();
 
-        // Add sample ingredients to list
-        localIngredientList.add(new Ingredient(1, "Apple", 80, 0.89,
-                "https://www.collinsdictionary.com/images/full/apple_158989157.jpg", "Fruit"));
-        localIngredientList.add(new Ingredient(2, "Rice", 200, 0.15,
-                "https://img.etimg.com/thumb/width-1200,height-900,imgsize-161058,resizemode-75,msid-102051894/nri/latest-updates/us-nris-bringing-home-dozens-of-rice-bags-as-india-bans-exports.jpg",
-                "Grains"));
-        localIngredientList.add(new Ingredient(3, "Eggplant", 40, 1.20,
-                "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcRjbf_1QWD0qZ7VP7X4bycHq-Rn_Xd7HXB5GVhD3jvrpXa5HO9WQh63en2UaITQTnUgFaZ-ArIGKgxQ0Ac",
-                "Vegetable"));
+
+        try (Scanner scanner = new Scanner(Paths.get("C:/Java/pantrywizard/src/main/resources/csv/samples.csv"))){
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                String[] parts = line.split(",");
+
+                int id = Integer.valueOf(parts[0]);
+                String name = parts[1];
+                int calories = Integer.valueOf(parts[2]);
+                double price = Double.parseDouble(parts[3]);
+                String imageURL = parts[4];
+                String category = parts[5];
+
+                localIngredientList.add(new Ingredient(id, name, calories, price, imageURL, category));
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error " + e.getLocalizedMessage());
+        }
+
+        ingredientRepository.saveAll(localIngredientList);
 
     }
 
